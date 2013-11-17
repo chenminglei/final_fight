8d8a3526f97837a46f9feae38f9c0cd929a3bbde
Di Li <chinalidi@ubuntu.(none)>
2013-11-16 18:31:04 -0800

    write sche.c

--- kernel/sched/sched.c
+++ kernel/sched/sched.c
@@ -22,7 +22,27 @@ tcb_t system_tcb[OS_MAX_TASKS]; /*allocate memory for system TCBs */
 
 void sched_init(task_t* main_task  __attribute__((unused)))
 {
-	
+	uint8_t i = 0;
+	uint8_t k = 0;
+	task_t tmp = null; 
+	for(i = 0;i < OS_MAX_TASKS;i ++) {
+	    for(k = OS_MAX_TASKS - 1;k >= i + 1;k --) {
+		if(main_task[k].T < main_task[k - 1].T) {
+	      	    tmp = main_task[k];
+		    main_task[k] = main_task[k - 1];
+		    main_task[k - 1] = tmp;     
+		}
+	    }
+	}
+	for(i = 0;i < OS_MAX_TASKS;i ++) {
+	    system_tcb[i].native_prio = i;
+	    system_tcb[i].cur_prio = i;
+	    system_tcb[i].context.r4 = (uint8_t)main_task[i].lambda; 
+	    system_tcb[i].context.r5 = (uint8_t)main_task[i].data;
+	    system_tcb[i].context.r6 = (uint8_t)main_task[i].stack_pos;
+	    system.tcb[i].sleep_queue = null;
+	    runqueue_add(&system_tcb[i], i);
+	}
 }
 
 /**
