d0e0ea4873b35bffd4530ed2d46f794a605074ee
Minglei Chen <chenzhuokb2@ubuntu.(none)>
2013-11-20 16:21:17 -0800

    change

--- kernel/arm/kernel.mk
+++ kernel/arm/kernel.mk
@@ -1,4 +1,4 @@
-ARM_OBJS := reg.o psr.o installHandler.o timeSetup.o userSetup.o irqSetup.o S_Handler.o int_asm.o irq_handler.o C_SWI_HANDLER.o init_interrupt.o
+ARM_OBJS := reg.o psr.o installHandler.o timeSetup.o userSetup.o S_Handler.o int_asm.o irq_handler.o C_SWI_HANDLER.o init_interrupt.o
 ARM_OBJS := $(ARM_OBJS:%=$(KDIR)/arm/%)
 
 KOBJS += $(ARM_OBJS)
