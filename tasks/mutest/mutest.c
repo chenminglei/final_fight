4d0e3153ec4dc1030658da1053e5da56869c7bdd
Minglei Chen <chenzhuokb2@ubuntu.(none)>
2013-11-23 12:15:12 -0800

    mutest

--- tasks/mutest/mutest.c
+++ tasks/mutest/mutest.c
@@ -17,10 +17,10 @@
 #include <unistd.h>
 #include <syscall.h>
 
-int sum = 0;
-int mutext_num = 0;
-int index1 = 0;
-int index2 = 0;
+unsigned int sum = 0;
+size_t mutext_num = 0;
+unsigned int index1 = 0;
+unsigned int index2 = 0;
 
 void panic(const char* str) {
 	puts(str);
@@ -28,7 +28,7 @@ void panic(const char* str) {
 }
 
 void fun1(void* str) {
-	for (index1 = 0; index1 < 50; ++index1) {
+        while (1) {
 		if (mutex_lock(mutext_num) == 0) {
 			++sum;
 			if (event_wait(0) < 0) {
@@ -36,12 +36,13 @@ void fun1(void* str) {
 			}
 			mutex_unlock(mutext_num);
 		}
+                index1++;
+	        printf("Index1: %u, Index2: %u, Sum: %u\n", index1, index2, sum);
 	}
-	printf("Index1: %d, Index2: %d, Sum: %d\n", index1, index2, sum);
 }
 
 void fun2(void* str) {
-	for (index2 = 0; index2 < 50; ++index2) {
+	while (1) {
 		if (mutex_lock(mutext_num) == 0) {
 			++sum;
 			if (event_wait(1) < 0) {
@@ -49,8 +50,9 @@ void fun2(void* str) {
 			}
 			mutex_unlock(mutext_num);
 		}
+                index2++;
+                printf("Index1: %u, Index2: %u, Sum: %u\n", index1, index2, sum);
 	}
-	printf("Index1: %d, Index2: %d, Sum: %d\n", index1, index2, sum);
 }
 
 int main(int argc, char** argv) {
@@ -77,9 +79,4 @@ int main(int argc, char** argv) {
 	puts("Why did your code get here!\n");
 	return 0;
 }
-
-
-
-
-
 
