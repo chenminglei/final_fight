fce9ecde1bf9a4ee7aae8e549182546a7c82867f
Minglei Chen <chenzhuokb2@ubuntu.(none)>
2013-11-24 13:23:15 -0800

    magic number

--- kernel/include/constant.h
+++ kernel/include/constant.h
@@ -5,21 +5,23 @@
  * Date:    7:47 PM  Nov 3th 2013                                             
  */ 
 
-#define VEC_SWI   0x08
-#define VEC_IRQ   0x18
-#define VEC_ADD   0x08
-#define LDR_INSTR 0xe51ff004
-#define LDR_CHECK 0xe59ff000
+#define VEC_SWI       0x08
+#define VEC_IRQ       0x18
+#define VEC_ADD       0x08
+#define LDR_INSTR     0xe51ff004
+#define LDR_CHECK     0xe59ff000
 #define LDR_DEL_DIST  0xfffff000
 #define LDR_GET_DIST  0x00000fff
 
 
 #define IRQ_STACK_SIZE 1024
+#define IRQ_BIT        0x80
+#define FIQ_BIT        0x50
+#define SWI_CODE_GET   0xff000000
 
-#define OSMR_COUNT  32500
+#define OSMR_COUNT      32500
 #define OSMR_ONE_SECOND 3250
-
-#define OSCR_CLEAR  0x00
+#define OSCR_CLEAR      0x00
 
 #define TIME_RESOLUTION 10
 
